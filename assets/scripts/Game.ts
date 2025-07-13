0086520eca9acdaaf87ebfdc0cd24c1bc527737d
onble <62949816+onble@users.noreply.github.com>
2025-07-13 21:01:17 +0800

    7.初始化游戏

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -1,14 +1,30 @@
-import { _decorator, Component, Node } from "cc";
+import { _decorator, Component, LabelComponent, Node, sys } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("Game")
 export class Game extends Component {
     @property(Node)
     startPanel: Node = null!;
+
     @property(Node)
     gamePanel: Node = null!;
+
     @property(Node)
     overPanel: Node = null!;
+
+    @property(LabelComponent)
+    txtLv: LabelComponent = null!;
+
+    @property(LabelComponent)
+    txtScore: LabelComponent = null!;
+
+    @property(LabelComponent)
+    txtBestScore: LabelComponent = null!;
+
+    @property(LabelComponent)
+    txtBack: LabelComponent = null!;
+
+    private userData: any = null;
 
     start() {
         this.initPanel();
@@ -23,10 +39,40 @@ export class Game extends Component {
         this.overPanel.active = false;
     }
 
+    // 初始化
+    private init() {
+        this.getUserInfo();
+        this.updateView();
+    }
+
+    // 获取用户信息
+    private getUserInfo() {
+        this.userData = JSON.parse(sys.localStorage.getItem("userData"));
+        if (this.userData == null) {
+            this.userData = {
+                lv: 5,
+                score: 0,
+                bestScore: 0,
+                array: [],
+                arr_histroy: [],
+                backNum: 3,
+            };
+        }
+    }
+
+    private updateView() {
+        let lv = this.userData.lv;
+        this.txtLv.string = lv + "x" + lv;
+        this.txtScore.string = this.userData.score.toString();
+        this.txtBestScore.string = this.userData.bestScore + "";
+        this.txtBack.string = "撤回(" + this.userData.backNum + ")";
+    }
+
     // 点击事件
     private onBtnStartClick() {
         this.initPanel();
         this.gamePanel.active = true;
+        this.init();
     }
 
     private onBtnRePlayClick() {}
